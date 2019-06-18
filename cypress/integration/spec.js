e524e33b08f481bbe362411fe4d46bb654c7bd95
Thoma Bigueres <thoma.bigueres@7circles.com>
2019-06-18 10:04:34 +0200

    [master] cypress test of input not working

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,7 @@
 describe('page', () => {
-  it('works', () => {})
+  it('notworking', () => {
+  	cy.visit("/reference/tags/input-number.html");
+  	cy.get("input[type=number][name=age]").type("3");
+
+  })
 })
